367fe4914df29bdf4bc7f3dcc7aabc7311119b5a
甜甜圈 <131566743+tian127168@users.noreply.github.com>
2024-06-10 11:43:28 +0800

    aa

---
+++ src/components/parts/Child.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <p>子组件</p>
+        <p>{{ msg }}</p>
+        <p>{{ msg1 }}</p>
+        <p><button @click="test('成功')">测试button</button></p>
+        <p><button @click="changs()">修改父组件传递的值</button></p>
+    </div>
+</template>
+
+<script>
+export default {
+    // props数组方式接收属性
+    // props:["msg","msg1","test"]
+    // props对象方式接收，推荐使用
+    props: {
+        // 表示接收的msg是一个string类型
+        msg: String,
+        msg1: {
+            type:[Number,String],
+            // 没有接收到msg1值时，会使用default值，即缺省值
+            // default:"初始值"
+            // 加了required：true表示父组件必须要传msg1
+            // required:true
+            // 自定义校验使用validator方法
+            // 参数为接收到的msg1的值
+            // 返回值如果为true，则校验通过
+            // 否则不通过
+            validator(val){
+               return (val>=0 &&val<=150)
+            }
+        },
+        test: Function
+    },
+    methods:{
+        changs(){
+            this.$emit("changs",45)
+        }
+    }
+
+}
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/components/parts/Parent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <p>父组件</p>
+        <!-- 传变量和方法需要使用v-bind来绑定 -->
+
+        <!-- @changs 监听事件 -->
+        <Child ref="ref1" msg="传递死数据" :msg1="msgVar" :test="test1" @changs="changm1"></Child>
+        <p ref="ref2">123123</p>
+        <p><button @click="ch()">点击获取</button></p>
+        <p ref="ref3">田某人写的hello wolrd</p>
+    </div>
+</template>
+
+<script>
+import Child from "./Child.vue"
+export default {
+   components:{
+    Child
+   },
+   data(){
+    return{
+        msgVar:90
+    }
+   },
+   methods:{
+    test1(str){
+        alert("测试方法" +str)
+    },
+    changm1(val){
+        this.msgVar=val;
+    },
+    ch(){
+        console.log(this);
+        console.log(this.$refs.ref1.msg)
+        
+    }
+   }
+}
+</script>
+
+<style lang="less" scoped></style>
